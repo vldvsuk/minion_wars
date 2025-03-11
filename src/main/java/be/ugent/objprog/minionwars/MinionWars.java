a8f258a54d0d94707188470c677ad7b54556c626
tibvdm <tibovandemoortele@hotmail.com>
2025-03-11 11:45:13 +0100

    template

---
+++ src/main/java/be/ugent/objprog/minionwars/MinionWars.java
@@ -0,0 +1,23 @@
+package be.ugent.objprog.minionwars;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MinionWars extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MinionWars.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+open module be.ugent.objprog.minionwars {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.jdom2;
+
+    exports be.ugent.objprog.minionwars;
+}
